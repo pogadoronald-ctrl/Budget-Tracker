249aa88311e62690f3886b70d17f13b85209650c
Ronald Pogado <pogado.ronald@gmail.com>
2026-07-31 04:51:50 +0800

    Added design for navbar

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,21 +1,52 @@
 import './Header.css'
+import { LuUser } from "react-icons/lu";
+
+
 
 function Header() {
     return (
-        <header className="header">
+        <header className="header shadow-md">
             <div className="logo">Logo</div>
 
-            <nav className="nav font-bold text-gray-700">
-                <a href="#">Overview</a>
-                <a href="#">Transactions</a>
-                <a href="#">Budgets</a>
-                <a href="#">Categories</a>
-                <a href="#">Budgets</a>
-                <a href="#">Recurring</a>
-            </nav>
+            <Pages />
 
-            <div className="profile">Profile</div>
+            <div className="profile">
+                <LuUser size={28} />
+            </div>
         </header>
+    )
+}
+
+const navLinks = [
+    "Overview", 
+    "Transactions",
+    "Budgets",
+    "Categories",
+    "Recurring"
+]
+
+const navHref = {
+
+}
+
+const Pages = () => {
+    return (
+        <nav className="nav font-bold text-gray-400">
+            {navLinks.map((link) => (
+                <a
+                    key={link}
+                    href=""
+                    className="relative text-gray-500 font-semibold 
+                        after:absolute after:left-0 after:-bottom-1 
+                        after:h-[2px] after:w-full after:bg-green-400 
+                        after:translate-y-2 after:opacity-0 
+                        hover:after:translate-y-0 hover:after:opacity-100 
+                        after:transition-all after:duration-300"
+                >
+                {link}
+                </a>
+            ))}
+        </nav>
     )
 }
 
