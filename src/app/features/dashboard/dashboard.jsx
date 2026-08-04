3a6e48305fd0c480f70c1a7548b47c13df09c09b
Ronald Pogado <pogado.ronald@gmail.com>
2026-08-04 12:17:57 +0800

    Added welcome message and add transaction button

--- src/app/features/dashboard/dashboard.jsx
+++ src/app/features/dashboard/dashboard.jsx
@@ -3,7 +3,8 @@ import {
     LuWallet,
     LuTrendingUp,
     LuTrendingDown,
-    LuPiggyBank
+    LuPiggyBank,
+    LuLayoutDashboard
 } from "react-icons/lu";
 
 import ChartCard from "../../components/ChartCard";
@@ -16,59 +17,81 @@ import BudgetLimitChart from "../../components/charts/BudgetLimitChart";
 
 function Dashboard() {
     return (
-        <div className="grid grid-cols-4 gap-6">
-            <SummaryCard
-                title="Current Balance"
-                amount="37,000"
-                type="balance"
-                icon={<LuWallet className="text-blue-500" />}
-            />
-            <SummaryCard
-                title="Total Income"
-                amount="10,000"
-                type="income"
-                icon={<LuTrendingUp className="text-green-500" />}
-            />
-            <SummaryCard
-                title="Total Expenses"
-                amount="5,000"
-                type="expenses"
-                icon={<LuTrendingDown className="text-red-500" />}
-            />
-            <SummaryCard
-                title="Net Savings"
-                amount="2,500"
-                type="savings"
-                icon={<LuPiggyBank className="text-yellow-500" />}
-            />
+        <div>
+            <div className="flex items-center justify-between mb-8">
 
-            <div className="col-span-4 grid grid-cols-2 gap-6">
-                <ChartCard >
-                    <IncomeExpenseChart />
-                </ChartCard>   
-                <ChartCard >
-                    <MonthlySpendingChart />
-                </ChartCard>
-                <div className="col-span-2 grid grid-cols-3 gap-6">
-                <div className="col-span-2">
-                    <ChartCard className="h-full">
-                        <RecentTransactions />
-                    </ChartCard>
+            <div className="flex items-center gap-3">
+                <div className="p-3 rounded-xl bg-blue-100">
+                    <LuLayoutDashboard className="text-2xl text-blue-600" />
                 </div>
 
-                <div className="flex flex-col gap-6">
-                    <ChartCard>
-                        <ExpenseCategoryChart />
-                    </ChartCard>
-
-                    <ChartCard>
-                        <BudgetLimitChart />
-                    </ChartCard>
+                <div>
+                    <h1 className="text-2xl font-bold">
+                        Welcome Back!
+                    </h1>
+                    <p className="text-sm text-gray-500">
+                        Here's a summary of your finance
+                    </p>
                 </div>
             </div>
-                
-            </div>
 
+            <button className="bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium px-5 py-3 rounded-xl shadow-sm">
+                Add Transaction
+            </button>
+        </div>
+
+            <div className="grid grid-cols-4 gap-6">
+                <SummaryCard
+                    title="Current Balance"
+                    amount="37,000"
+                    type="balance"
+                    icon={<LuWallet className="text-blue-500" />}
+                />
+                <SummaryCard
+                    title="Total Income"
+                    amount="10,000"
+                    type="income"
+                    icon={<LuTrendingUp className="text-green-500" />}
+                />
+                <SummaryCard
+                    title="Total Expenses"
+                    amount="5,000"
+                    type="expenses"
+                    icon={<LuTrendingDown className="text-red-500" />}
+                />
+                <SummaryCard
+                    title="Net Savings"
+                    amount="2,500"
+                    type="savings"
+                    icon={<LuPiggyBank className="text-yellow-500" />}
+                />
+
+                <div className="col-span-4 grid grid-cols-2 gap-6">
+                    <ChartCard >
+                        <IncomeExpenseChart />
+                    </ChartCard>   
+                    <ChartCard >
+                        <MonthlySpendingChart />
+                    </ChartCard>
+                    <div className="col-span-2 grid grid-cols-3 gap-6">
+                        <div className="col-span-2">
+                            <ChartCard className="h-full">
+                                <RecentTransactions />
+                            </ChartCard>
+                        </div>
+
+                        <div className="flex flex-col gap-6">
+                            <ChartCard>
+                                <ExpenseCategoryChart />
+                            </ChartCard>
+
+                            <ChartCard>
+                                <BudgetLimitChart />
+                            </ChartCard>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
